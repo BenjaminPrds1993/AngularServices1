eb7e188df33b12fdcd5627c21eb9c68a056d7eb6
Benjamin Prades <benjaminprades@orange.fr>
2019-04-15 13:30:35 +0200

    maj2

--- src/app/cocktail.service.ts
+++ src/app/cocktail.service.ts
@@ -9,11 +9,11 @@ export class CocktailService {
 
   getCocktails() {
     return [
-      {name: " Cocktail1", info : "blablabla"},
-      {name: " Cocktail2", info : "blablabla"},
-      {name: " Cocktail3", info : "blablabla"},
-      {name: " Cocktail4", info : "blablabla"},
-      {name: " Cocktail5", info : "blablabla"}
+      {name: " Cocktail1", price : "blablabla", img:'http://i.f1g.fr/media/ext/1900x1900/madame.lefigaro.fr/sites/default/files/img/2009/04/12287034jpg.jpg',},
+      {name: " Cocktail2", price : "blablabla", img:'http://i.f1g.fr/media/ext/1900x1900/madame.lefigaro.fr/sites/default/files/img/2009/04/12287034jpg.jpg',},
+      {name: " Cocktail3", price : "blablabla", img:'http://i.f1g.fr/media/ext/1900x1900/madame.lefigaro.fr/sites/default/files/img/2009/04/12287034jpg.jpg',},
+      {name: " Cocktail4", price : "blablabla", img:'http://i.f1g.fr/media/ext/1900x1900/madame.lefigaro.fr/sites/default/files/img/2009/04/12287034jpg.jpg',},
+      {name: " Cocktail5", price : "blablabla", img:'http://i.f1g.fr/media/ext/1900x1900/madame.lefigaro.fr/sites/default/files/img/2009/04/12287034jpg.jpg',}
     ];
   }
 }
